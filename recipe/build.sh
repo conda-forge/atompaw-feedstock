fdc357242cd9405eacb0a8d0a20549f179c1f578
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-31 09:56:19 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ./configure --prefix=$PREFIX \
+   --enable-libxc --with-libxc-incs="-I${PREFIX}/include" --with-libxc-libs="-L${PREFIX}/lib -lxc" \
    --enable-shared --disable-static \
    --with-linalg-libs="-L${PREFIX}/lib -lblas -llapack -lpthread"
 
